44122f9b89889db06e5b0632b1c09749b0ece269
Abhishek Kargeti1 <abhishek.kargeti1@gmail.com>
2024-06-21 10:06:55 +0530

    Js added

---
+++ FrontEnd Work/js/script.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+// var a=parseInt(prompt("Enter a number"));
+// var b=parseInt(prompt("Enter a number"));
+// var result= a+b;
+// console.log(result); 
+
+
+// var a=+prompt("Enter a number");
+// var b=+prompt("Enter a number");
+// var result= a+b;
+// console.log(result); 
+
+
+// length=parseInt(prompt("Enter a length"))
+// Width=parseInt(prompt("Enter a width"))
+// result= length*Width;
+// console.log(result)
+
+// dollar= (prompt("Enter your salary in dollar"))
+// Rupee=dollar*83.43;
+// console.log(Rupee)
